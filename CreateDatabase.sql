4cfbe37bb17c59fd9469657dcb785172228bf530
Cyber Superior <cybersuperior1@gmail.com>
2020-11-29 11:13:07 -0600

    Finalize create database sql script

--- CreateDatabase.sql
+++ CreateDatabase.sql
@@ -2,6 +2,7 @@
 SHOW DATABASES;
 
 -- Create schema/database with name 'example'
+-- Can create schema in MySQL Workbench through right clicking in schema menu and selecting 'Create Schema'
 CREATE SCHEMA `example` ;
 
 -- Change database so that following queries would run on database named 'example'
