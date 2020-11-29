d815fdc8408ff80208b1e2b1dba9fadef4e8c62d
Cyber Superior <cybersuperior1@gmail.com>
2020-11-29 10:54:32 -0600

    Add create database helper SQL script

---
+++ CreateDatabase.sql
@@ -0,0 +1,9 @@
+-- View all the databases on your MySQL Server instance
+SHOW DATABASES;
+
+-- Create schema/database with name 'example'
+CREATE SCHEMA `example` ;
+
+-- Change database so that following queries would run on database named 'example'
+-- Can change database in MySQL Workbench by double clicking schema name
+USE example;
